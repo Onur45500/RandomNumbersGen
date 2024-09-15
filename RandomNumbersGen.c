221eab7b5088d6fdbe402516aae3a90db4d84866
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-15 09:28:54 +0200

    Implemented a random function with a max parameter and displayed it

--- RandomNumbersGen.c
+++ RandomNumbersGen.c
@@ -1,3 +1,28 @@
 
 /* RandomNumbersGen.c */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>  
+
+int random_function(int max)
+{
+	int x;
+	x = rand() % max + 1;
+	return x;
+}
+
+int main()
+{
+	int random;
+	
+	printf("%d\n", getpid());	
+
+	srand(getpid());	
+	random = random_function(10);
+	
+	printf("Your random number %d\n", random);
+
+	return 0;
+}
